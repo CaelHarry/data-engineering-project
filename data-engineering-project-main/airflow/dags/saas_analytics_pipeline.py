741c572cefec11abee62aed3e493ce70b188a857
Cael Harry <caelharry@berkeley.edu>
2026-02-02 10:32:49 -0800

    updated dag, first node working, docker-compose correct, need to write a script to create user on startup/ save user somehow

--- data-engineering-project-main/airflow/dags/saas_analytics_pipeline.py
+++ data-engineering-project-main/airflow/dags/saas_analytics_pipeline.py
@@ -14,27 +14,27 @@ with DAG(
 
     generate_data = BashOperator(
         task_id="generate_data",
-        bash_command=f"python {PROJECT_DIR}/generators/saas-generator.py"
+        bash_command=f"cd {PROJECT_DIR} && python generators/saas-generator.py"
     )
 
     ingest_raw = BashOperator(
         task_id="ingest_raw_events",
-        bash_command=f"python {PROJECT_DIR}/ingestion/load_raw.py"
+        bash_command=f"cd {PROJECT_DIR} && python ingestion/load_raw.py"
     )
 
     dbt_run_staging = BashOperator(
         task_id="dbt_run_staging",
-        bash_command=f"cd {PROJECT_DIR}/postgres_proj/models && dbt run --select staging"
+        bash_command=f"cd {PROJECT_DIR}/postgres_proj && dbt run --select staging"
     )
 
     dbt_run_marts = BashOperator(
         task_id="dbt_run_marts",
-        bash_command=f"cd {PROJECT_DIR}/postgres_proj/models && dbt run --select marts"
+        bash_command=f"cd {PROJECT_DIR}/postgres_proj && dbt run --select marts"
     )
 
     dbt_test = BashOperator(
         task_id="dbt_test",
-        bash_command=f"cd {PROJECT_DIR}/postgres_proj/tests && dbt test"
+        bash_command=f"cd {PROJECT_DIR}/postgres_proj && dbt test"
     )
 
     generate_data >> ingest_raw >> dbt_run_staging >> dbt_run_marts >> dbt_test
